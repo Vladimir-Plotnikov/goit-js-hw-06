9546f317bb6e61276c554b173cf48198a82c1e60
Volodymyr <vovdata@gmail.com>
2022-10-21 22:05:33 +0200

    Update task-02.js
    task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const refs = {
+  staticItems: document.querySelector('#ingredients')
+}
+
+refs.staticItems.classList.add('item')
+const liArray = []
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  const li = document.createElement('li')
+  const ingredient = ingredients[i];
+  li.textContent = ingredient
+  liArray.push(li)
+}
+
+refs.staticItems.append(...liArray)
