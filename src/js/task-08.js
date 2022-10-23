3d87b0bf7147a66721a5080247dce11a3ec24051
Volodymyr <vovdata@gmail.com>
2022-10-23 16:08:31 +0200

    Update task-08.js
    8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,4 +1,19 @@
 const refs = {
 loginForm: document.querySelector('.login-form'),
 }
-console.log(refs);
+// console.log(refs.loginForm);
+
+refs.loginForm.addEventListener('submit',onFormSubmit)
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        alert("всі поля мають бути заповнені");
+    }
+    console.log(`email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
