995b7638c6f93a9a2ae972f964fcb2f9e183f702
Volodymyr <vovdata@gmail.com>
2022-10-24 07:03:11 +0200

    Update task-09.js
    123321

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,40 @@
+const changeColoBtnRef = document.querySelector('.change-color');
+const spanRef = document.querySelector('.color');
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+function handleChangeColor() {
+  const color = getRandomHexColor();
+  document.body.setAttribute('style', `background-color: ${color};`);
+  spanRef.textContent = color;
+}
+
+changeColoBtnRef.addEventListener('click', handleChangeColor);
+
+
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+// const refs = {
+//   body: document.querySelector('body'),
+//   buttonChange: document.querySelector('button'),
+// }
+// let spanColor = document.querySelector('.color')
+
+
+// refs.buttonChange.addEventListener('click', changeColor)
+// refs.buttonChange.addEventListener('click',changeName)
+
+// function changeColor (event) {
+//   const color = getRandomHexColor()
+// spanColor.textContent = color;
+//   refs.body.style.backgroundCOlor = color;
+//   event.currentTarget = color;
+
+// }
+
